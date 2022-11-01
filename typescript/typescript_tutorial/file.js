83afa110f0e830dcf9ce4284d7e2e9791911b00f
Abdul Rehman <114314272+Maani5790@users.noreply.github.com>
2022-11-01 19:21:28 +0500

    Add files via upload

---
+++ typescript/typescript_tutorial/file.js
@@ -0,0 +1,8 @@
+// console.log("hello world");
+function akcent() {
+    for (var i = 0; i < 5; i++) {
+        console.log(i);
+    }
+    console.log("C typescript Python Js" + i);
+}
+akcent();
---
+++ typescript/typescript_tutorial/file.ts
@@ -0,0 +1,9 @@
+// console.log("hello world");
+function akcent(){
+    for(var i=0; i<5; i++){
+        console.log(i);
+    }
+    console.log("C typescript Python Js" + i)
+}
+
+akcent();
